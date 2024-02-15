0ca3fcd9e7cf214b46a8ee21c05f758612a0fb01
Omer Eisenberg <omereis@yahoo.com>
2024-02-15 09:14:23 +0200

    New Commit

---
+++ main.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace RmvDose
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.dlgOpenCsv = new System.Windows.Forms.OpenFileDialog();
+			this.txtbxFile = new System.Windows.Forms.TextBox();
+			this.gridCSV = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.gridCSV)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(12, 27);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(75, 23);
+			this.btnOpen.TabIndex = 1;
+			this.btnOpen.Text = "&Open...";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// dlgOpenCsv
+			// 
+			this.dlgOpenCsv.DefaultExt = "*.csv";
+			this.dlgOpenCsv.Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
+			// 
+			// txtbxFile
+			// 
+			this.txtbxFile.Location = new System.Drawing.Point(319, 64);
+			this.txtbxFile.Name = "txtbxFile";
+			this.txtbxFile.Size = new System.Drawing.Size(323, 20);
+			this.txtbxFile.TabIndex = 2;
+			// 
+			// gridCSV
+			// 
+			this.gridCSV.AllowUserToAddRows = false;
+			this.gridCSV.AllowUserToDeleteRows = false;
+			this.gridCSV.AllowUserToOrderColumns = true;
+			this.gridCSV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+			this.gridCSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.gridCSV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+			this.gridCSV.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+			this.gridCSV.Location = new System.Drawing.Point(12, 77);
+			this.gridCSV.Name = "gridCSV";
+			this.gridCSV.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+			this.gridCSV.RowHeadersVisible = false;
+			this.gridCSV.Size = new System.Drawing.Size(579, 351);
+			this.gridCSV.TabIndex = 3;
+			// 
+			// Column1
+			// 
+			this.Column1.HeaderText = "Date Time";
+			this.Column1.Name = "Column1";
+			// 
+			// Column2
+			// 
+			this.Column2.HeaderText = "Device";
+			this.Column2.Name = "Column2";
+			// 
+			// Column3
+			// 
+			this.Column3.HeaderText = "Rate";
+			this.Column3.Name = "Column3";
+			// 
+			// Column4
+			// 
+			this.Column4.HeaderText = "Dose";
+			this.Column4.Name = "Column4";
+			// 
+			// Column5
+			// 
+			this.Column5.HeaderText = "Status";
+			this.Column5.Name = "Column5";
+			// 
+			// main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.gridCSV);
+			this.Controls.Add(this.txtbxFile);
+			this.Controls.Add(this.btnOpen);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "main";
+			this.Text = "RMVC Dose Calculator";
+			((System.ComponentModel.ISupportInitialize)(this.gridCSV)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog dlgOpenCsv;
+        private System.Windows.Forms.TextBox txtbxFile;
+		private System.Windows.Forms.DataGridView gridCSV;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+	}
+}
+
---
+++ main.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RmvDose
+{
+    public partial class main : Form
+    {
+//-----------------------------------------------------------------------------
+        public main()
+        {
+            InitializeComponent();
+        }
+//-----------------------------------------------------------------------------
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dlgOpenCsv.FileName = "";
+            if (dlgOpenCsv.ShowDialog() == DialogResult.OK) {
+                CsvToGrid (dlgOpenCsv.FileName, gridCSV);
+                txtbxFile.Text = dlgOpenCsv.FileName;
+            }
+        }
+//-----------------------------------------------------------------------------
+        private void CsvToGrid (string strFileName, DataGridView grid) {
+            
+        }
+//-----------------------------------------------------------------------------
+    }
+}
